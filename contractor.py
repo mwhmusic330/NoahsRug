00492bf3b8c5b793e4e661224857b096676e3c14
mwhmusic330 <m.w.haburay.music@gmail.com>
2026-02-24 16:16:39 +0000

    the contractor - initial mask doesnt work yet

--- contractor.py
+++ contractor.py
@@ -4,9 +4,21 @@ import pandas as pd
 
 df = pd.read_csv('/home/michael/NoahsRug/noahs-orders.csv')
 
+customers_df = pd.read_csv('/home/michael/NoahsRug/noahs-customers.csv')
+
+def initials(name):
+    return ''.join(word[0] for word in name.split()).upper()
+
+customers_df['initials'] = customers_df['name'].apply(initials)
+
+init_find = 'JP'
+
+init_mask = customers_df['initials'] == init_find
+
 df['ordered'] = pd.to_datetime(df['ordered'])
 
 year_filter = df[df['ordered'].dt.year == 2017]
 
 print(year_filter)
+print(init_mask)
 
