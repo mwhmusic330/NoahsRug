ae6c9ebcdc32bb057355aa5832cf774bb04014b7
mwhmusic330 <m.w.haburay.music@gmail.com>
2026-02-26 19:01:12 +0000

    update 2-26

--- contractor.py
+++ contractor.py
@@ -15,7 +15,7 @@ customers_df['initials'] = customers_df['name'].apply(initials)
 
 init_find = 'JP'
 
-init_mask = customers_df['initials'] == init_find
+init_mask = str(customers_df['initials']) == init_find
 
 df['ordered'] = pd.to_datetime(df['ordered'])
 
@@ -26,3 +26,4 @@ print(init_mask)
 
 
 #### Function works to get initials i tested by printing customers_df['initials'] but i am still getting all falses on comparison. Thinking a middle initial may be involved. Starting on that work around.
+#### 2-26 Still looking at this tried changing the comparison string and ensuring that the output of the initials column is a string with str() method I'm just not sure why I keep getting falses? Unlss I'm reading the probem wrong theres gotta be initials 'JP' in here.
