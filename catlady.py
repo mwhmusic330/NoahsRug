66ba216d59b45c0a709356e53961980c665ae177
mwhmusic330 <m.w.haburay.music@gmail.com>
2026-04-16 20:25:38 +0000

    start of catlady

--- catlady.py
+++ catlady.py
@@ -10,3 +10,21 @@ df_products = pd.read_csv('/home/michael/NoahsRug/noahs-products.csv')
 sku_df  = pd.read_csv('/home/michael/NoahsRug/noahs-orders_items.csv')
 
 cus_df  = pd.read_csv('/home/michael/NoahsRug/noahs-customers.csv')
+
+location = "Staten Island, NY"
+
+location_mask = cus_df['citystatezip'].str.startswith(location)
+
+location_filter = cus_df[location_mask]
+
+search_prefix = 'COL'
+
+sku_mask = df_products['sku'].str.contains(search_prefix)
+sku_filter = df_products[sku_mask]
+
+desc_mask = sku_filter['desc'].str.contains('Jersey')
+desc_filter = sku_filter[desc_mask]
+
+
+print(desc_filter)
+
